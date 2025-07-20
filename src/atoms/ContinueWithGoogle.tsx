83d2b56f8c82c0635aa027dab71dbf74f3824f1d
unknown <eilaylevi95@gmail.com>
2025-07-20 16:38:02 +0300

    [fix] when signup with google password, name and phone not neccesery

--- src/atoms/ContinueWithGoogle.tsx
+++ src/atoms/ContinueWithGoogle.tsx
@@ -2,17 +2,17 @@ import { CredentialResponse, GoogleLogin } from '@react-oauth/google';
 import { jwtDecode } from "jwt-decode";
 import { useNavigate } from 'react-router-dom';
 
+
 interface ContinueWithGoogleProps {
   style?: React.CSSProperties;
-  navigation: String;
+  navigation: () => void;
 }
 
-export const ContinueWithGoogle = ({style, navigation}: ContinueWithGoogleProps) => {
-  const navigate = useNavigate();
-      const handleLoginSuccess = (credentialResponse: CredentialResponse) => {
+export const ContinueWithGoogle: React.FC<ContinueWithGoogleProps> = ({ style, navigation }) => {
+  const handleLoginSuccess = (credentialResponse: CredentialResponse) => {
     if (credentialResponse.credential) {
       const decoded: any = jwtDecode(credentialResponse.credential);
-      navigate(String(navigation))
+      navigation();
       console.log("Decoded user:", decoded);
     } else {
       console.error("No credential found.");
@@ -20,12 +20,12 @@ export const ContinueWithGoogle = ({style, navigation}: ContinueWithGoogleProps)
   };
   return (
     <div style={style}>
-        <GoogleLogin
-            onSuccess={handleLoginSuccess}
-            onError={() => {
-              console.log('Login Failed');
-            }}
-            />
+      <GoogleLogin
+        onSuccess={handleLoginSuccess}
+        onError={() => {
+          console.log('Login Failed');
+        }}
+      />
     </div>
-  )
-}
+  );
+};
